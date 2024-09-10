80965daf87004998460bdb97564ea2a8bbe86ce8
lionel-imac <flf241892@sina.cn>
2024-09-10 20:45:22 +0800

    contact page change

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -50,7 +50,7 @@ const Contact = () => {
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
-          <div className="xl:h-[54%] order-2 xl:order-none">
+          <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Let's work together</h3>
               <p className="text-white/60">
